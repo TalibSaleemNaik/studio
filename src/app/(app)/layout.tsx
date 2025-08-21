8a59ecf014c0cf34a8b85e4c4bda342fdf9f51ef
Big Bee <bigb35953t@gmail.com>
2025-08-21 20:16:49 +0000

    another problem is page size when we add new column it go out of the pag

--- src/app/(app)/layout.tsx
+++ src/app/(app)/layout.tsx
@@ -25,7 +25,7 @@ function AppLayout({
             <MainNav />
           </SidebarContent>
         </Sidebar>
-        <div className="flex flex-col sm:gap-4 sm:py-4 sm:pl-72">
+        <div className="flex flex-col sm:gap-4 sm:py-4 sm:pl-72 min-w-0">
            <header className="sticky top-0 z-30 flex h-14 items-center gap-4 border-b bg-background px-4 sm:static sm:h-auto sm:border-0 sm:bg-transparent sm:px-6">
              <SidebarTrigger />
              <div className="ml-auto flex items-center gap-2">
