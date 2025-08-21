c9a64000a5344b9ee3484b57887c7cd112367927
Big Bee <bigb35953t@gmail.com>
2025-08-21 20:17:55 +0000

    not updated

--- src/app/(app)/layout.tsx
+++ src/app/(app)/layout.tsx
@@ -25,14 +25,14 @@ function AppLayout({
             <MainNav />
           </SidebarContent>
         </Sidebar>
-        <div className="flex flex-col sm:gap-4 sm:py-4 sm:pl-72 min-w-0">
-           <header className="sticky top-0 z-30 flex h-14 items-center gap-4 border-b bg-background px-4 sm:static sm:h-auto sm:border-0 sm:bg-transparent sm:px-6">
+        <div className="flex flex-col sm:gap-4 sm:py-4 sm:pl-72 flex-1 min-w-0">
+           <header className="sticky top-0 z-30 flex h-14 items-center gap-4 border-b bg-background px-4 sm:static sm:h-auto sm:border-0 sm:bg-transparent sm:px-6 shrink-0">
              <SidebarTrigger />
              <div className="ml-auto flex items-center gap-2">
                 <UserNav />
              </div>
            </header>
-           <main className="grid flex-1 items-start gap-4 p-4 sm:px-6 sm:py-0 md:gap-8">
+           <main className="flex flex-col flex-1 gap-4 p-4 sm:px-6 sm:py-0 md:gap-8 min-w-0">
              {children}
            </main>
         </div>
